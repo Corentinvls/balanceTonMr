b06c08954b97be4dae9235fa9f9152375c7f42f9
Corentin Vallois <vallois@macbook-pro.edu.sir>
2020-10-07 16:42:51 +0200

    it works cochon kebab projet

--- src/Services/GitlabServices.php
+++ src/Services/GitlabServices.php
@@ -4,6 +4,8 @@
 namespace App\Services;
 
 
+use App\Entity\Projects;
+use Doctrine\ORM\EntityManagerInterface;
 use Gitlab\Client;
 
 
@@ -11,19 +13,34 @@ class GitlabServices
 
 {
     private $client;
+    private $entityManager;
 
 
-    public function __construct(Client $client)
+    public function __construct(Client $client, EntityManagerInterface $entityManager)
     {
         $this->client = $client;
+        $this->entityManager = $entityManager;
     }
 
-    public function getAllProjects() {
-        return $this->client->projects()->all(["owned"=>true]);
+    public function getAllProjects()
+    {
+        $allProjects = $this->client->projects()->all(["owned" => true]);
+
+        foreach ($allProjects as $project) {
+            $newProject = new Projects();
+             $newProject->setGitLabId($project['id']);
+            $newProject->setName($project['name']);
+            $this->entityManager->persist($newProject);
+            $this->entityManager->flush();
+        }
+
+        return $allProjects;
+
     }
 
 
-    public function getMergeRequestFromProject (int $project_id) {
+    public function getMergeRequestFromProject(int $project_id)
+    {
         return $this->client->mergeRequests()->all($project_id);
     }
 
