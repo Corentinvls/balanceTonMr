b3e3b14a5baf48ec6a5892dffe07ee6595e70daa
Corentin Vallois <vallois@macbook-pro.edu.sir>
2020-10-09 11:35:15 +0200

    done

--- src/Command/EmailNotificationCommand.php
+++ src/Command/EmailNotificationCommand.php
@@ -1,45 +1,29 @@
 <?php
-
 namespace App\Command;
-
+use App\Services\MailService;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Style\SymfonyStyle;
-
 class EmailNotificationCommand extends Command
 {
-    protected static $defaultName = 'emailNotification';
-
+    protected static $defaultName = 'email';
+    /**
+     * @var MailService
+     */
+    private $mailService;
+    public function __construct( MailService $mailService)
+    {
+        $this->mailService = $mailService;
+        parent::__construct();
+    }
     protected function configure()
     {
-        $this
-            ->setDescription('Add a short description for your command')
-            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
-            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
-        ;
+        $this->setDescription('Send email.....');
     }
-
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $io = new SymfonyStyle($input, $output);
-        $arg1 = $input->getArgument('arg1');
-
-        if ($arg1) {
-            $io->note(sprintf('You passed an argument: %s', $arg1));
-        }
-
-        if ($input->getOption('option1')) {
-            // ...
-        }
-
-        /**
-         * Appel de MailService ici
-         */
-        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
-
+        $this->mailService->sendNotifications();
+        $output->writeln('Email send');
         return Command::SUCCESS;
     }
 }
